8af01faa773f75c319fce11af60506430085019b
|   Ascension   | <theodorekyle.choa.0315@gmail.com>
2025-09-05 12:31:57 +0800

    O(n)

---
+++ Septembet_5_2025.java
@@ -0,0 +1,10 @@
+public class Septembet_5_2025 {
+    public static void main(String[] args) {
+    int n = 10; // Count as 1 operation 
+    if (n > 5) { // Count as 1 operation (comparison)
+        System.out.println("The number is greater than 5."); // Count as 1 operation
+    } else { // Branch does not execute (0 operation)
+        System.out.println("The number is 5 or less."); 
+    }
+    }
+} 
