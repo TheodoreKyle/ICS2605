dde9c85077085e5b8b3c64db2627b781ded655d4
|   Ascension   | <theodorekyle.choa.0315@gmail.com>
2025-09-08 12:07:36 +0800

    Binary Searching - Asynchronous Activity

--- Choa_BinarySearch.java
+++ Choa_BinarySearch.java
@@ -3,18 +3,33 @@ public class Choa_BinarySearch {
         int i = 0, j = A.length - 1; // i = low, j = high
         while (i <= j) {
             int m = (int) Math.floor((i + j) / 2); // m = mid of array
-            if (A[m] == x) return m; 
-            else if (x > A[m]) i = m + 1; 
-            else j = m - 1; 
+            for (int a = i; a <= j; a++) System.out.print(A[a] + " ");
+            System.out.println();  
+
+            if (A[m] == x) return m; // O(1)
+            else if (x > A[m]) i = m + 1; // going to the right of middle element
+            else j = m - 1; // going to the left of middle element
         }
-        return -1; 
+        return -1;  
     }
     public static void main(String[] args) {
-        char[] A = {'A', 'G', 'H', 'I', 'L', 'M', 'O', 'R', 'T'}; 
-        char x = 'H', y = 'K'; 
-        if (BinarySearch(x, A) == -1) System.out.println(x + " is not in the array.");
-        else System.out.println(x + " is found at index: " + BinarySearch(x, A));
-        if (BinarySearch(y, A) == -1) System.out.println(y + " is not in the array.");
-        else System.out.println(y + " is found at index: " + BinarySearch(y, A)); 
+        char[] A = {'A', 'G', 'H', 'I', 'L', 'M', 'O', 'R', 'T'};  
+        char x = 'K';   
+        int result = BinarySearch(x, A);
+        if (result == -1) System.out.println(x + " is not in the array."); 
+        else System.out.println(x + " is found at index: " + result);   
     }
 }
+
+/* y = 'K'; 
+int resultX = BinarySearch(x, A);
+    if (resultX != -1) 
+        System.out.println(x + " is found at index: " + resultX);
+    else 
+        System.out.println(x + " is not in the array."); 
+
+    int resultY = BinarySearch(y, A);
+    if (resultY != -1) 
+        System.out.println(y + " is found at index: " + resultY); 
+    else 
+        System.out.println(y + " is not in the array."); */ 
