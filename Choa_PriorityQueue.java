0022bbf49a9f6e503a401195965425852661d534
|   Ascension   | <theodorekyle.choa.0315@gmail.com>
2025-10-11 21:54:41 +0800

    Priority Queue

--- Choa_PriorityQueue.java
+++ Choa_PriorityQueue.java
@@ -1,9 +1,10 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 // Natural Ordering
 
 public class Choa_PriorityQueue {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); 
         pq.add(50);
         pq.add(10);
         pq.add(30);
