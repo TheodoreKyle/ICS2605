fc7c77703c2d67c1400568d78ca4b0fa0875785f
|   Ascension   | <theodorekyle.choa.0315@gmail.com>
2025-12-03 08:40:48 +0800

    Graph Lab Discussion

---
+++ December 3, 2025/Graph.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList; 
+public class Graph {
+    private LinkedList<String>[] adjList;
+    private int size;
+
+    public Graph(int size) {
+        this.size = size;
+        adjList = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            adjList[i] = new LinkedList<>(); 
+        }
+    }
+
+    private int index(char v) {
+        return v - 'A';
+    }
+
+    public void addEdge(char src, char dest) {
+        adjList[index(src)].add(String.valueOf(dest));
+        adjList[index(dest)].add(String.valueOf(src)); 
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < size; i++) {
+            char vertex = (char) (i + 'A');
+            System.out.print(vertex + ": ");
+            
+            for (String neighbor : adjList[i]) {
+                System.out.print("-> " + neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(4); // A, B, C, D
+
+        graph.addEdge('A', 'B');
+        graph.addEdge('A', 'C');
+        graph.addEdge('B', 'D');
+        graph.addEdge('C', 'D');
+
+        graph.printGraph();
+    }
+}
